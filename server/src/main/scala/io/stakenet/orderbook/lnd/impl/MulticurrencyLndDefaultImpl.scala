e65d5961477bb91953a2f2c8a72dfced652c2540
Philipp Müller <mail@pm92.de>
2022-03-08 10:51:11 +0100

    fsdf

--- server/src/main/scala/io/stakenet/orderbook/lnd/impl/MulticurrencyLndDefaultImpl.scala
+++ server/src/main/scala/io/stakenet/orderbook/lnd/impl/MulticurrencyLndDefaultImpl.scala
@@ -100,7 +100,7 @@ class MulticurrencyLndDefaultImpl @Inject() (clientBuilder: LightningClientBuild
       .withMemo(memo)
       .withValue(amount.valueFor(currency).longValue)
       .withExpiry(7200)
-      .withCltvExpiry(160)
+      .withCltvExpiry(360)
 
     clientBuilder
       .getLnd(currency)
