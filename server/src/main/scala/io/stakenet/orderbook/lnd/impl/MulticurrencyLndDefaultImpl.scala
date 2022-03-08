9f35c4c67ba3f28252b91a2db74a225313bc11e0
Philipp Müller <mail@pm92.de>
2022-03-08 09:48:42 +0100

    change cltv increase

--- server/src/main/scala/io/stakenet/orderbook/lnd/impl/MulticurrencyLndDefaultImpl.scala
+++ server/src/main/scala/io/stakenet/orderbook/lnd/impl/MulticurrencyLndDefaultImpl.scala
@@ -100,7 +100,7 @@ class MulticurrencyLndDefaultImpl @Inject() (clientBuilder: LightningClientBuild
       .withMemo(memo)
       .withValue(amount.valueFor(currency).longValue)
       .withExpiry(7200)
-      .withCltvExpiry(120)
+      .withCltvExpiry(160)
 
     clientBuilder
       .getLnd(currency)
